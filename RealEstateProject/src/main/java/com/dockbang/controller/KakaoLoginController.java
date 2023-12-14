55fe78d975d936784683f572d1c675c821fc756a
kangsoong <94055161+kangsoong@users.noreply.github.com>
2023-12-14 12:19:43 +0900

    Feat: KakaoController 추가
    - 세션 등록 추가

--- RealEstateProject/src/main/java/com/dockbang/controller/KakaoLoginController.java
+++ RealEstateProject/src/main/java/com/dockbang/controller/KakaoLoginController.java
@@ -1,9 +1,16 @@
 package com.dockbang.controller;
 
+import com.dockbang.mapper.SqlMapperInter;
 import com.dockbang.model.KakaoDAO;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+
+import org.apache.catalina.Server;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
@@ -24,20 +31,31 @@ import java.util.Map;
 @Slf4j
 @RestController
 @RequestMapping("")
+@MapperScan(basePackages = {"com.dockbang.mapper"})
 public class KakaoLoginController {
 
     @Value("${kakao.client_id}")
     private String client_id;
 
     @Autowired
-    private KakaoDAO kakaoService;
+    private KakaoDAO kakaoService;    
+
+	@Autowired
+	private SqlMapperInter mapper;
 
     @GetMapping("/callback")
-    ModelAndView callback(@RequestParam("code") String code) throws IOException {
+    ModelAndView callback(@RequestParam("code") String code, HttpServletRequest request) throws IOException {
         String accessToken = kakaoService.getAccessTokenFromKakao(client_id, code);
-        HashMap<String, Object> userInfo = kakaoService.getUserInfo(accessToken);
+        HashMap<String, Object> userInfo = kakaoService.getUserInfo(accessToken);        
         // User 로그인, 또는 회원가입 로직 추가
-        System.out.println(userInfo.toString());
+        int flag = kakaoService.setKakaoUser(userInfo);
+        
+        // 로그인이 정상적으로 되면 세션 생성
+        if(flag == 2||flag ==1) {
+        	HttpSession session = request.getSession();
+        	session.setAttribute("email",userInfo.get("email").toString());
+        	session.setAttribute("nickname",userInfo.get("nickname").toString());
+        }
         
         // view(.jsp) 설정
  		ModelAndView modelAndView = new ModelAndView();
