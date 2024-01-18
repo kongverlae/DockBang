45ea12418e70af8e6cab5aef1249e1070288741f
minsoo Kim <51105967+MinsooKim99@users.noreply.github.com>
2024-01-18 13:33:25 +0900

    Chore: SqlMapperInter.java
    - 테스트코드 제거

--- RealEstateProject/src/main/java/com/dockbang/mapper/SqlMapperInter.java
+++ RealEstateProject/src/main/java/com/dockbang/mapper/SqlMapperInter.java
@@ -145,7 +145,7 @@ public interface SqlMapperInter {
 	// saleTO 참고
 	//@Select("select title, lat, lon from sale")
 	//@Select("select title, lat, lon, sale_type from sale")
-	@Select("select * from sale limit 500")
+	@Select("select * from sale")
 	List<SaleTO> getSales();
 	
 	// saleTO 참고
